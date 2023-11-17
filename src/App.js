3cab9612c1a64fb190bf262bc4dff33eb10384fc
Asmit-Pandey <asmitpandey070@gmail.com>
2023-11-17 21:08:48 +0530

    Login & Registration form Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//pages
 import Home from "./pages/home.js";
 import Login from "./pages/login";
 import Register from "./pages/register";
 
 function App() {
+  const { user } = useSelector((state) => ({...state}));
   return (
     <div className="dark">
       <Router>
