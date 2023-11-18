5eec6f92dec14f3574f6312b1687e66c8066ea05
Asmit-Pandey <asmitpandey070@gmail.com>
2023-11-18 13:18:56 +0530

    Home setup, sidebar, etc in progress

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { persistReducer , persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userSlice from "../features/userSlice";
 import createFilter from "redux-persist-transform-filter";
+import chatSlice from "../features/chatSlice";
 
 //saveUserOnlyFilter
 const saveUserOnlyFilter= createFilter("user",["user"]);
@@ -18,6 +19,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userSlice,
+    chat: chatSlice,
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
